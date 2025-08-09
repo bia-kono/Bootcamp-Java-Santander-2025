a2e5e10896459690f8d2d75b8a86f4a1550c7351
bia-kono <bia-1994@live.com>
2025-08-08 22:50:46 -0300

    Primeiro commit: Adicionando estrutura inicial do projeto

---
+++ modulo_basico/src/main/java/br/est/bianca/bc_java/Main.java
@@ -0,0 +1,13 @@
+package br.est.bianca.bc_java;
+
+public class Main {
+
+    private String message = "Hello World!";
+
+    public void setMessage() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
